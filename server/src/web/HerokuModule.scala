d2f2c8e4fe4084628fff6bf30d74f1b8022dcf73
Adu Bhandaru <adu@thrivenow.com>
2019-06-03 22:02:55 -0700

    Use 0.0.0.0 host when trying to boot on heroku

--- server/src/web/HerokuModule.scala
+++ server/src/web/HerokuModule.scala
@@ -4,4 +4,5 @@ object HerokuModule {
   val DefaultPort = 8080
   val EnvironmentPort = sys.env.get("PORT").map(_.toInt)
   val Port = EnvironmentPort.getOrElse(DefaultPort)
+  val Host = "0.0.0.0"
 }
--- server/src/web/WebServer.scala
+++ server/src/web/WebServer.scala
@@ -1,25 +1,27 @@
 package monarchy.web
 
 import akka.actor.ActorSystem
+import akka.event.Logging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.directives.DebuggingDirectives
 import akka.stream.ActorMaterializer
-import scala.io.StdIn
 
 object WebServer extends App {
-  implicit val system = ActorSystem("web-system")
+  implicit val system = ActorSystem("monarchy-web")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
   implicit val queryCli = DatabaseModule.queryClient
   val port = HerokuModule.Port
-  val route = {
+  val route = logRequestResult("monarchy-web") {
     pathSingleSlash {
       complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
     }
   }
-  val routeBindings = Http().bindAndHandle(route, "localhost", port)
+  val routeLogged = DebuggingDirectives.logRequestResult("LOG:", Logging.InfoLevel)(route)
+  val routeBindings = Http().bindAndHandle(routeLogged, HerokuModule.Host, port)
 
-  println(s"Server online at $port")
+  println(s"web-server online at port=$port")
 }
