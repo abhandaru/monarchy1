503401f30e6e1da0c30cce9b2c6f65754ad941f6
Adu Bhandaru <abhandaru@x1.co>
2024-01-08 00:44:50 -0500

    fix tests

---
+++ server/src/test-util/Ids.scala
@@ -0,0 +1,9 @@
+package monarchy.testutil
+
+import java.util.UUID
+
+object Ids {
+  val A = UUID.fromString("00000000-0000-0000-0000-000000000001")
+  val B = UUID.fromString("00000000-0000-0000-0000-000000000002")
+  val C = UUID.fromString("00000000-0000-0000-0000-000000000003")
+}
